5352c90fe6e0984f0d86172382d8acdb09368b27
aditya.bhendavadekar <adityab1193@gmail.com>
2025-01-30 23:10:58 +0530

    commiting into

--- Session2.java
+++ Session2.java
@@ -1,6 +1,89 @@
+// import java.util.Scanner;
+
 class Session2{
     public static void main(String[] args){
-        System.out.println("hello");
-        int a=10;
+
+
+        System.out.println(args[0].toString()+" "+ args[1].toString());
+
+
+        // System.out.println("hello");
+        // Scanner sc = new Scanner(System.in);
+
+        // Switch case
+
+        // int day = sc.nextInt();  // 2
+
+        // switch(day){
+            
+        //     case 1: System.out.println("Monday");
+        //             break;
+            
+        //     case 2: System.out.println("Tuesday");
+        //             break;
+
+        //     // case 7
+
+        //     default:
+        //     System.out.println("wrong input");
+        // }
+
+        // String
+    // simple 
+        // String str = "hello";
+
+        // String h = "hello";
+
+        // by using new keyword
+        // String str2 = new String("here");
+
+        // System.out.println(str);
+
+        // System.out.println(str2);
+
+
+        // String str = "hello";
+
+        // // input 
+        // String str2 = sc.nextLine();
+
+
+        // System.out.println("str2: "+str2);
+
+        // System.out.println(str.charAt(3)+" \n"+ 
+        // str.toUpperCase()+" "+
+        // str.toLowerCase());
+
+        // StringBuilder sb = new StringBuilder("hello there");
+        // System.out.println("\nOriginal: "+sb);
+
+        // sb.append(" here");
+        // System.out.println("\n\n"+sb);
+
+        // sb.replace(0, 4, "my");
+        // System.out.println("\n\n"+sb);
+
+        // sb.delete(0,2);
+        // System.out.println("\n\n"+sb);
+
+
+        // int []a = new int[5];
+
+        // // a[0] =2;
+        // // a[1]=23;
+
+        // // a[0] = sc.nextInt();
+
+        // for(int i=0;i<a.length;i++){
+        //     a[i] = sc.nextInt();
+        // }
+
+        // System.out.println("Array elements are: ");
+        // for(int i: a){
+        //     System.out.println(i);
+        // }
+        
+
+
     }
 }
